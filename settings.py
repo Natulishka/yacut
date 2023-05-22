cdcf8ce8498c2122a439ee1bc3837b2f4656e363
Natalya Shulgina <natulishka84@gmail.com>
2023-05-22 20:37:47 +0300

    SQLALCHEMY_DATABASE_URI

--- settings.py
+++ settings.py
@@ -4,6 +4,6 @@ LEN_SHORT_ID = 6
 
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI', 'sqlite:///db.sqlite3')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY')
